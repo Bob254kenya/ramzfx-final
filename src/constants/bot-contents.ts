59b09699d073d21d3f099cb3066b1f061766e121
Robert Mose Nyaundi <robertnyaundi09@gmail.com>
2026-06-30 22:10:37 +0300

    Update bot-contents.ts

--- src/constants/bot-contents.ts
+++ src/constants/bot-contents.ts
@@ -20,6 +20,7 @@ export const DBOT_TABS: TDashboardTabIndex = Object.freeze({
     MANUAL_TRADING: 5,
     SCANNER: 6,
     ANALYSIS_TOOL: 7,
+    SPEED_BOT: 8,
 });
 
 export const MAX_STRATEGIES = 10;
@@ -33,6 +34,7 @@ export const TAB_IDS = [
     'id-manual-trading',
     'id-scanner',
     'id-analysistool',
+    'id-speed-bot',
 ];
 
 export const DEBOUNCE_INTERVAL_TIME = 500;
